61d90b791da2405f5a92ab022569a37ae29712ad
Aimarl0 <Aimarl0@163.com>
2015-11-20 01:41:31 +0800

    lesson 4

--- MaterialStudy/app/src/main/java/com/example/aimar/materialstudy/MainActivity.java
+++ MaterialStudy/app/src/main/java/com/example/aimar/materialstudy/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.aimar.materialstudy;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab);
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab1"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab2"));
+
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab3"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab4"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab5"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab6"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab7"));
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        mTabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+//        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                Log.d(TAG, "onTabSelected() called with: " + tab.getText());
+//            }
 //
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//                Log.d(TAG, "onTabUnselected() called with: " + tab.getText());
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//                Log.d(TAG, "onTabReselected() called with: " + tab.getText());
+//            }
+//        });
+
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
